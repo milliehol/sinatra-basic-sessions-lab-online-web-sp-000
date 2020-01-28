1bc6c24e0ed4d6340e3816ba4696b24d253aad7b
Carla Holcomb <carla.programmer@gmail.com>
2020-01-28 01:17:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,5 +11,11 @@ class App < Sinatra::Base
     @session = session
     erb :index
   end
+  
+    post '/checkout' do
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+   end
 
 end
