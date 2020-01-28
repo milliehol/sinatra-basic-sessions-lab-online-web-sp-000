870022a312cd31438b0f5799774fffd1b9e5fddd
Carla Holcomb <carla.programmer@gmail.com>
2020-01-28 01:19:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+  end
+  
+  get '/' do 
+    @session = session
+    erb :index
+  end
+  
+  post '/checkout' do
+    @sessions = session
+    item = params["item"] #sets the params in the session hash
+    @sessions[:item] = item #displays the shopping cart contents
+  end
+
 end
